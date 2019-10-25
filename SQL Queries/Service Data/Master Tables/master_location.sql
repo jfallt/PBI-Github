1efb56e66a62c69b4c9c0265d9219eccefa55668
Jared Fallt <jfallt@quenchonline.com>
2019-10-25 10:56:29 -0400

    Update master_location.sql
    format and add technician id

--- SQL Queries/Service Data/Master Tables/master_location.sql
+++ SQL Queries/Service Data/Master Tables/master_location.sql
@@ -1,12 +1,12 @@
-SELECT DISTINCT
-	Name AS 'Zip Code',
-	--City__c as 'City',
-	--[State__c] as 'State',
-	--Country__c as 'Country',
-	Region__c as 'Region',
-	Submarket__c as 'Submarket',
-	MSA_CSA__c as 'Market',
-        [SLA_Terms__c] 'SLA',
-        [Service_Coverage__c] as 'Service Coverage',
-        [Filtration__c] as 'Filtration'
-FROM [Temporal].[ZipCode]
+SELECT DISTINCT Name AS 'Zip Code'
+	,Region__c as Region
+	--City__c as City,
+	--[State__c] as State,
+	--Country__c as Country,
+	,Submarket__c as Submarket
+	,MSA_CSA__c as Market
+	,SLA_Terms__c SLA
+	,Service_Coverage__c as 'Service Coverage'
+	,Filtration__c as Filtration
+	,Technician__c as TechnicianId
+FROM Temporal.ZipCode z
