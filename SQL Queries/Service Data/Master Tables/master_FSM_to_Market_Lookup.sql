ff6c99e6c1a4ddee16cad16afe348e9f52599b7f
Jared Fallt <48260710+jfallt@users.noreply.github.com>
2019-12-05 11:59:23 -0500

    Update master_FSM_to_Market_Lookup.sql
    change variables to temp table

--- SQL Queries/Service Data/Master Tables/master_FSM_to_Market_Lookup.sql
+++ SQL Queries/Service Data/Master Tables/master_FSM_to_Market_Lookup.sql
@@ -1,9 +1,45 @@
-WITH Markets AS
+DROP TABLE IF EXISTS #FSMs
+DROP TABLE IF EXISTS #FSM2
+DROP TABLE IF EXISTS #Region
+
+CREATE TABLE #FSMs
+	(
+		Service_Member_Name VARCHAR(100),
+		Region VARCHAR(100)
+	)
+
+INSERT INTO #FSMs (Service_Member_Name, Region) VALUES ('Waldy Negron', 'NE'), ('Jose Marte', 'NE')
+														, ('Rich Delucia', 'NE'), ('Curt Braverman', 'NE')
+														, ('Dale Hanks', 'SE'), ('Daren Killingsworth', 'SE')
+														, ('Alicia Jones', 'SE'),('Alan Parker', 'SE')
+														, ('Thomas Kern', 'SE'), ('Steve Hliwski', 'SE')
+														, ('Toby Hopkins', 'SE'), ('Keith Lloyd', 'SE')
+														, ('Julio Curiel', 'C'), ('Steve Weisenberger', 'C')
+														, ('Mario Mendez', 'C'), ('Daryl Linville', 'C')
+														, ('Arthur Perez', 'C'), ('Jason Healy', 'C')
+														, ('Ricky Smith', 'C'), ('Eris Esparza', 'W')
+														, ('Andrea De La Cruz', 'W'), ('James Harber', 'W')
+														, ('Atilio Duenas', 'W'), ('Rudy Gonzales', 'W')
+														, ('Fabian Lopez', 'W')
+CREATE TABLE #Region
+	(
+		Region_Key VARCHAR(100),
+		Region VARCHAR(100)
+	)
+
+INSERT INTO #Region (Region_Key, Region) VALUES ('NE', 'Northeast'), ('SE', 'Southeast'), ('C', 'Central'), ('W', 'West')
+														
+
+SELECT *, IDENTITY (int, 1,1) AS id
+INTO #FSM2
+FROM #FSMs
+
+;WITH Markets AS
 
 (
 	SELECT DISTINCT
 	[MSA_CSA__c] as 'Market'
-	FROM [Source].[Zip_Code__c]
+	FROM [Temporal].[ZipCode]
 ),
 
 FSMtoMarket AS
@@ -12,110 +48,67 @@ FSMtoMarket AS
 	SELECT
 		CASE
 			WHEN Market = 'Manhattan, NY'
-			THEN 'Waldy Negron'
-
+			THEN (SELECT Service_Member_Name FROM #FSM2 WHERE id = 1)
 			WHEN Market IN ('Outer Boroughs, NY', 'White Plains, NY', 'Trenton, NJ')
-			THEN 'Jose Ventura'
-
+			THEN (SELECT Service_Member_Name FROM #FSM2 WHERE id = 2)
 			WHEN Market IN ('Boston-Cambridge -NH-VT-ME')
-			THEN 'Rich Delucia'
-
+			THEN (SELECT Service_Member_Name FROM #FSM2 WHERE id = 3)
 			WHEN Market IN ('Boston Suburbs-West-RI', 'Hartford, CT')
-			THEN 'Curt Braverman'
-
+			THEN (SELECT Service_Member_Name FROM #FSM2 WHERE id = 4)
 			WHEN Market = 'Los Angeles - Orange County'
-			THEN 'Eris Esparza'
-
+			THEN (SELECT Service_Member_Name FROM #FSM2 WHERE id = 20)
 			WHEN Market = 'San Diego - Bakersfield - Inland Empire'
-			THEN 'Andrea De La Cruz'
-
+			THEN (SELECT Service_Member_Name FROM #FSM2 WHERE id = 21)
 			WHEN Market IN ('Phoenix, AZ', 'Denver, CO', 'Tucson, AZ')
-			THEN 'James Harber'
-
+			THEN (SELECT Service_Member_Name FROM #FSM2 WHERE id = 22)
 			WHEN Market IN ('Las Vegas, NV', 'Portland, OR', 'Seattle, WA', 'Las Vegas,NV')
-			THEN 'Rudy Gonzales'
-
+			THEN (SELECT Service_Member_Name FROM #FSM2 WHERE id = 24)
 			WHEN Market = 'San Francisco, CA'
-			THEN 'Atilio Duenas'
-
+			THEN (SELECT Service_Member_Name FROM #FSM2 WHERE id = 23)
 			WHEN Market IN ('Washington, DC', 'Baltimore, MD', 'Alexandria, VA')
-			THEN 'Alicia Jones'
-
+			THEN (SELECT Service_Member_Name FROM #FSM2 WHERE id = 7)
 			WHEN Market = 'Atlanta, GA'
-			THEN 'Keith Lloyd'
-
+			THEN (SELECT Service_Member_Name FROM #FSM2 WHERE id = 12)
 			WHEN Market = 'Raleigh, NC'
-			THEN 'Alan Parker'
-
+			THEN (SELECT Service_Member_Name FROM #FSM2 WHERE id = 8)
 			WHEN Market IN ('Tampa, FL', 'Jacksonville, FL', 'Orlando, FL','Fort Myers, FL', 'Ft Myers')
-			THEN 'Dale Hanks'
-			
+			THEN (SELECT Service_Member_Name FROM #FSM2 WHERE id = 5)
 			WHEN Market = 'Miami, FL'
-			THEN 'Thomas Kern'
-
+			THEN (SELECT Service_Member_Name FROM #FSM2 WHERE id = 9)
 			WHEN Market IN ('Philadelphia, PA', 'Wilmington, DE')
-			THEN 'Steve Hliwski'
-
+			THEN (SELECT Service_Member_Name FROM #FSM2 WHERE id = 10)
 			WHEN Market IN ('Detroit, MI', 'Syracuse, NY', 'Pittsburgh, PA', 'Cincinnati, OH', 'Cleveland, OH', 'Columbus, OH', 'Dayton, OH')
-			THEN 'Steve Weisenberger'
-
+			THEN (SELECT Service_Member_Name FROM #FSM2 WHERE id = 14)
 			WHEN Market IN ('Chicago, IL', 'Milwaukee, WI')
-			THEN 'Mario Mendez'
-
+			THEN (SELECT Service_Member_Name FROM #FSM2 WHERE id = 15)
 			WHEN Market IN ('Birmingham, AL', 'Nashville, TN', 'Memphis, TN', 'New Orleans, LA')
-			THEN 'Ricky Smith'
-
+			THEN (SELECT Service_Member_Name FROM #FSM2 WHERE id = 19)
 			WHEN Market IN ('Indianapolis, IN', 'Louisville, KY', 'Kansas City, MO', 'Minneapolis, MN', 'St. Louis, MO')
-			THEN 'Daryl Linville'
-
+			THEN (SELECT Service_Member_Name FROM #FSM2 WHERE id = 16)
 			WHEN Market IN ('Houston, TX')
-			THEN 'Arthur Perez'
-
+			THEN (SELECT Service_Member_Name FROM #FSM2 WHERE id = 17)
 			WHEN Market IN ('Austin, TX', 'San Antonio, TX', 'El Paso, TX', 'St. Louis, MO')
-			THEN 'Jason Healy'
-
+			THEN (SELECT Service_Member_Name FROM #FSM2 WHERE id = 18)
 			WHEN Market IN ('Dallas, TX', 'Oklahoma City, OK')
-			THEN 'Julio Curiel'
-
+			THEN (SELECT Service_Member_Name FROM #FSM2 WHERE id = 13)
 			WHEN Market = 'Salt Lake City, UT'
-			THEN 'Fabian Lopez'
-
+			THEN (SELECT Service_Member_Name FROM #FSM2 WHERE id = 25)
 			WHEN Market IN ('Charlotte, NC', 'Richmond, VA', 'Virginia Beach, VA', 'Hilton Head, SC')
-			THEN 'Toby Hopkins'
-
+			THEN (SELECT Service_Member_Name FROM #FSM2 WHERE id = 11)
 			WHEN Market IN ('Augusta, GA', 'Savannah, GA', 'Macon, GA')
-			THEN 'Daren Killingsworth '
-
+			THEN (SELECT Service_Member_Name FROM #FSM2 WHERE id = 6)
 			WHEN Market IN ('ROW', 'Outsourced')
 			THEN 'N/A'
-
 			ELSE 'Needs Assignment'
 			END as 'FSM',
 		Market
 	FROM Markets
-),
-
-FSMtoRegion AS
-(
-SELECT
-*,
-CASE
-	WHEN FSM IN('Daryl Linville', 'Jason Healy', 'Julio Curiel', 'Mario Mendez', 'Ricky Smith', 'Steve Weisenberger', 'Arthur Perez')
-	THEN 'Central'
-
-	WHEN FSM IN('Curt Braverman', 'Rich Delucia', 'Jose Ventura', 'Waldy Negron')
-	THEN 'Northeast'
-
-	WHEN FSM IN('Alan Parker', 'Alicia Jones', 'Thomas Kern', 'Daren Killingsworth', 'Keith Lloyd', 'Steve Hliwski', 'Toby Hopkins', 'Dale Hanks')
-	THEN 'Southeast'
-
-	WHEN FSM IN('Andrea De La Cruz', 'Eris Esparza', 'Atilio Duenas', 'Fabian Lopez', 'James Harber', 'Rudy Gonzales', 'Toby Hopkins')
-	THEN 'West'
-
-	ELSE 'ROW'
-	END as 'Region'
-FROM FSMtoMarket
 )
 
-SELECT * FROM FSMtoRegion
-ORDER BY Market
+SELECT FSM
+	,Market
+	,ISNULL(r.Region, 'ROW') as Region
+FROM FSMtoMarket f2m
+LEFT JOIN #FSM2 f on f.Service_Member_Name = f2m.FSM
+LEFT JOIN #Region r on r.Region_Key = f.Region
+
