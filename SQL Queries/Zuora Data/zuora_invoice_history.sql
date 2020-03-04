aac98e1c0e32669dd59f5d8b255f2980c9569b26
Jared Fallt <48260710+jfallt@users.noreply.github.com>
2020-03-04 09:41:10 -0500

    Update zuora_invoice_history.sql
    dynamic updating

--- SQL Queries/Zuora Data/zuora_invoice_history.sql
+++ SQL Queries/Zuora Data/zuora_invoice_history.sql
@@ -1,28 +1,84 @@
-DECLARE @asofdate as Date
-SET @asofdate = '2019-10-01'
+------------------------------------------------------------------------------------------------------
+---------------------Create temp table to store results of while loop---------------------------------														
+------------------------------------------------------------------------------------------------------
+DROP TABLE IF EXISTS #overdue_history
 
-;WITH Past_Due_Invoices AS
-(
-SELECT @asofdate as AsOfDate
-	,zi.Account_Number__c as Account_Number
-    ,zi.[Name] as Invoice_Number
-    ,LegacyInvoiceNumber__c as Legacy_Invoice_Number
-    ,CAST(Zuora__GeneratedDate__c as DATE) as Invoice_Date
-	,DATEADD(d, 91, CAST(Zuora__DueDate__c as DATE)) as Roll_Date -- add 91 days to due date for roll
-    ,Zuora__Balance2__c as Balance
-	,CAST(Zuora__DueDate__c as DATE) as Invoice_Due_Date
-	,Zuora__Age_Bucket__c
-	,zi.Number_of_Days_Passed__c
-	,DATEDIFF(d
-			,CAST(Zuora__DueDate__c as DATE)
-			,CAST(@asofdate as DATE)
-			) as Days_Overdue
-	,[Zuora__PaymentTerm__c] as Payment
-	,zi.Zuora__Account__c
-FROM Temporal.ZuoraZInvoice FOR SYSTEM_TIME AS OF @asofdate zi
-WHERE Zuora__Balance2__c <> 0
-)
+CREATE TABLE #overdue_history (
+					 AsOfDate datetime
+					,Account_Number nvarchar (1300)
+					,Invoice_Number nvarchar (80)
+					,Legacy_Invoice_Number nvarchar	(255)
+					,Invoice_Date datetime
+					,Roll_Date datetime
+					,Balance float
+					,Invoice_Due_Date datetime
+					,Zuora__Age_Bucket__c nvarchar	(1300)
+					,Number_of_Days_Passed__c float
+					,Days_Overdue float
+					,Payment nvarchar (32)
+					,Zuora__Account__c nvarchar	(18)
+					,Total_Amount float
+					);
+------------------------------------------------------------------------------------------------------
+----------------Declare variables, only looking at beginning date of quarter--------------------------														
+------------------------------------------------------------------------------------------------------
+DECLARE @quarter_offset as int
+SET @quarter_offset = 0
 
+DECLARE @asofdate datetime
+SET @asofdate = DATEADD(qq, DATEDIFF(qq, 0, GETDATE()) - @quarter_offset, 0)
+
+------------------------------------------------------------------------------------------------------
+--------------------------------------Begin while loop------------------------------------------------														
+------------------------------------------------------------------------------------------------------
+WHILE DATEADD(qq, DATEDIFF(qq, 0, GETDATE()) - @quarter_offset, 0) > '04-01-2019'  
+
+BEGIN
+
+	INSERT INTO #overdue_history (
+					 AsOfDate
+					,Account_Number
+					,Invoice_Number
+					,Legacy_Invoice_Number
+					,Invoice_Date
+					,Roll_Date
+					,Balance
+					,Invoice_Due_Date
+					,Zuora__Age_Bucket__c
+					,Number_of_Days_Passed__c
+					,Days_Overdue
+					,Payment
+					,Zuora__Account__c
+					,Total_Amount
+					)
+
+	SELECT CAST(@asofdate AS DATE)
+		,zi.Account_Number__c
+		,zi.[Name]
+		,LegacyInvoiceNumber__c
+		,CAST(Zuora__InvoiceDate__c as DATE)
+		,DATEADD(d, 91, CAST(Zuora__DueDate__c as DATE)) -- add 91 days to due date for roll
+		,Zuora__Balance2__c as Balance
+		,CAST(Zuora__DueDate__c as DATE)
+		,Zuora__Age_Bucket__c
+		,zi.Number_of_Days_Passed__c
+		,DATEDIFF(d
+				,CAST(Zuora__DueDate__c as DATE)
+				,CAST(@asofdate as DATE)
+				)
+		,[Zuora__PaymentTerm__c]
+		,zi.Zuora__Account__c
+		,Zuora__TotalAmount__c as Total_Amount
+	FROM Temporal.ZuoraZInvoice FOR SYSTEM_TIME AS OF @asofdate zi
+	WHERE Zuora__Balance2__c <> 0
+
+	SET @quarter_offset = @quarter_offset + 1; -- add one day to asofdate
+	SET @asofdate = DATEADD(qq, DATEDIFF(qq, 0, GETDATE()) - @quarter_offset, 0)
+
+END
+------------------------------------------------------------------------------------------------------
+----------------------------Apply Aging Transformation------------------------------------------------
+------------------------------------------------------------------------------------------------------
 SELECT AsOfDate
 	,Zuora__Account__c as Account_Id
 	,Invoice_Number
@@ -43,11 +99,13 @@ SELECT AsOfDate
 		 ELSE 'No Aging'
 	 END as Revised_Aging
 	 ,Roll_Date
-	 ,CASE
+	,CASE
 		WHEN Days_Overdue > 90 THEN 0
-		WHEN DATEPART(QUARTER, Roll_Date) = DATEPART(QUARTER, @asofdate)
-		AND DATEPART(YEAR, Roll_Date)= DATEPART(YEAR, @asofdate)
+		WHEN DATEPART(QUARTER, Roll_Date) = DATEPART(QUARTER, AsOfDate)
+		AND DATEPART(YEAR, Roll_Date)= DATEPART(YEAR, AsOfDate)
 		THEN 1
 		ELSE 0
 		END as Roll
- FROM Past_Due_Invoices
+	,Total_Amount
+ FROM #overdue_history
+ WHERE asofdate <> CAST(GETDATE() as DATE)
