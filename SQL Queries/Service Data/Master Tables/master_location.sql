19b4bea04996d50442dfc0e68c72336736b0bac6
Jared Fallt <48260710+jfallt@users.noreply.github.com>
2020-03-04 09:43:19 -0500

    Update master_location.sql
    add city, state and country back in as we are driving all location based data from this table in service_data.pbix

--- SQL Queries/Service Data/Master Tables/master_location.sql
+++ SQL Queries/Service Data/Master Tables/master_location.sql
@@ -1,12 +1,13 @@
-SELECT DISTINCT Name AS 'Zip Code'
-	,Region__c as Region
-	--City__c as City,
-	--[State__c] as State,
-	--Country__c as Country,
-	,Submarket__c as Submarket
-	,MSA_CSA__c as Market
-	,SLA_Terms__c SLA
-	,Service_Coverage__c as 'Service Coverage'
-	,Filtration__c as Filtration
-	,Technician__c as TechnicianId
+SELECT DISTINCT z.Name AS 'Zip Code'
+	,z.Region__c as Region
+	,z.City__c as City
+	,z.[State__c] as State
+	,z.Country__c as Country
+	,z.Submarket__c as Submarket
+	,z.MSA_CSA__c as Market
+	,z.SLA_Terms__c SLA
+	,z.Service_Coverage__c as 'Service Coverage'
+	,z.Filtration__c as Filtration
+	,sgm.Name as Technician
 FROM Temporal.ZipCode z
+	LEFT JOIN Temporal.SVMXCServiceGroupMembers sgm on sgm.id = z.Technician__c
